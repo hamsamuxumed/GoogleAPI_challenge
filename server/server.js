a345c91f63fd7b7a62640b1a0f838424ad60c637
hamsamuxumed <hamsamuxumed3@gmail.com>
2021-09-28 14:57:27 +0100

    add search array

--- server/server.js
+++ server/server.js
@@ -5,9 +5,13 @@ const port = 3000;
 
 //app.use(cors()); 
 
+const search = [{
+  cars: 'Used Cars For Sale | AutoTrader', description: 'Used cars for sale, including private sales and local dealers near you. Find the right used car for you on Auto Trader - the UKs top destination.'
+}]
+
 app.get('/', (req, res) => res.send('Welcome to (not)Google search'));
 
 //Start server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  })
+  console.log(`Server running at http://localhost:${port}`);
+})
